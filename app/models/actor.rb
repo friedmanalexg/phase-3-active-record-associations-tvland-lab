151f56cb4d18481c893cb0af29cebcc6500012e6
Alex <friedmanalexg@gmail.com>
2022-08-17 14:56:47 -0400

    commit tree 71c727e269bfc4a68509e17f661011848640a00d

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,15 @@
 class Actor < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :shows, through: :characters
+
+    def full_name
+        names_array = [self.first_name,self.last_name]
+        names_array.join(" ")
+    end
+
+    def list_roles
+        self.characters.map {|c| "#{c.name} - #{c.show}"}
+        #still doesn't work
+        
+    end
 end
